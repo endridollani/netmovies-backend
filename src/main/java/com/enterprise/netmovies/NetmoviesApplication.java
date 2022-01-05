fd9918db144355665b8506f7a7b06974403781a0
Endri Dollani <endridollani99@gmail.com>
2022-01-05 21:54:15 +0100

    Initializing working environment, done

---
+++ src/main/java/com/enterprise/netmovies/NetmoviesApplication.java
@@ -0,0 +1,13 @@
+package com.enterprise.netmovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetmoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetmoviesApplication.class, args);
+	}
+
+}
