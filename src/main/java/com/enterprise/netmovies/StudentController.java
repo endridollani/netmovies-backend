ca54f7da84a09da78dfa7851041142798282aae4
Endri Dollani <endridollani99@gmail.com>
2022-01-06 01:48:16 +0100

    Enabled access from all client by doing @CrossOrigin(*). Just for testing purposes.

--- src/main/java/com/enterprise/netmovies/StudentController.java
+++ src/main/java/com/enterprise/netmovies/StudentController.java
@@ -1,6 +1,7 @@
 package com.enterprise.netmovies;
 
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/students")  //http://localhost:8080/api/v1/students
+@CrossOrigin("*")
 @AllArgsConstructor
 
 public class StudentController {
