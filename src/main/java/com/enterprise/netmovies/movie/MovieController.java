9eb5825018052559851defb7fc1b3712e5fdcd05
Endri Dollani <endridollani99@gmail.com>
2022-01-30 23:03:49 +0100

    small additions

--- src/main/java/com/enterprise/netmovies/movie/MovieController.java
+++ src/main/java/com/enterprise/netmovies/movie/MovieController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:8081")
 @RequestMapping("api/v1/movies")
 @AllArgsConstructor
 public class MovieController {
--- src/main/java/com/enterprise/netmovies/user/UserController.java
+++ src/main/java/com/enterprise/netmovies/user/UserController.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:8081")
 @RequestMapping("/api/v1/user")
+
 public class UserController {
     @Autowired
     private UserService userService;
