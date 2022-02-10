cdacde3aae743c292e6a325560f324fe471717ce
Endri Dollani <endridollani99@gmail.com>
2022-02-10 21:52:04 +0100

    Implemented Authority model

---
+++ src/main/java/com/enterprise/netmovies/models/Authority.java
@@ -0,0 +1,42 @@
+package com.enterprise.netmovies.models;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+import org.springframework.security.core.GrantedAuthority;
+
+@Data
+@Document(collection = "AUTH_AUTHORITY")
+public class Authority implements GrantedAuthority {
+    @Id
+    private String id;
+
+    @Field(name = "ROLE_CODE")
+    private String roleCode;
+
+    @Field(name = "ROLE_DESCRIPTION")
+    private String roleDescription;
+
+    @Override
+    public String getAuthority() {
+        return roleCode;
+    }
+
+
+    public String getRoleCode() {
+        return roleCode;
+    }
+
+    public void setRoleCode(String roleCode) {
+        this.roleCode = roleCode;
+    }
+
+    public String getRoleDescription() {
+        return roleDescription;
+    }
+
+    public void setRoleDescription(String roleDescription) {
+        this.roleDescription = roleDescription;
+    }
+}
--- src/main/java/com/enterprise/netmovies/models/User.java
+++ src/main/java/com/enterprise/netmovies/models/User.java
@@ -1,15 +1,17 @@
 package com.enterprise.netmovies.models;
 
 
-import com.mongodb.client.model.Collation;
+import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import java.time.ZonedDateTime;
+import java.util.Collection;
+import java.util.Date;
 
+@Data
 @Document(collection = "AUTH_USER_DETAILS")
 
 public class User implements UserDetails {
@@ -18,13 +20,11 @@ public class User implements UserDetails {
     @Id
     private String id;
 
-
-
     @Indexed(unique = true)
     private String userName;
     private String password;
-    private ZonedDateTime createdAt;
-    private ZonedDateTime updatedAt;
+    private Date createdAt;
+    private Date updatedAt;
 
     private String firstName;
     private String lastName;
@@ -34,60 +34,38 @@ public class User implements UserDetails {
 
 
     @Override
-    public Collection<? extends GrantedAuthority> getAuthorities(){
+    public Collection<? extends GrantedAuthority> getAuthorities() {
         return null;
     }
-    
-//    public User(){
-//
-//    }
-//
-//    public User(String firstName, String lastName, String email, String password){
-//        this.firstName = firstName;
-//        this.lastName = lastName;
-//        this.email = email;
-//        this.password = password;
-//    }
 
-//    public String getPassword() {
-//        return password;
-//    }
-//
-//    public void setPassword(String password) {
-//        this.password = password;
-//    }
-//
-//    public String getFirstName() {
-//        return firstName;
-//    }
-//
-//    public void setFirstName(String firstName) {
-//        this.firstName = firstName;
-//    }
-//
-//    public String getLastName() {
-//        return lastName;
-//    }
-//
-//    public void setLastName(String lastName) {
-//        this.lastName = lastName;
-//    }
-//
-//    public String getEmail() {
-//        return email;
-//    }
-//
-//    public void setEmail(String email) {
-//        this.email = email;
-//    }
-//
-//    public String  getId() {
-//        return id;
-//    }
-//
-//    public void setId(String id) {
-//        this.id = id;
-//    }
+    @Override
+    public String getPassword() {
+        return null;
+    }
 
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
 
 }
